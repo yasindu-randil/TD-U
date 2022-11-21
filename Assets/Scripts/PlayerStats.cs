b64426619d7c4834f2681a81252ebd10e4705b29
yasindu-randil <yr.yasindu@gmail.com>
2022-11-21 16:51:15 -0600

    Change player stats configuration

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,13 +6,13 @@ public class PlayerStats : MonoBehaviour
 {
     public static int Money;
     public int startMoney = 450;
-
+    public int lives_max = 100;
     public static int Lives;
     public int startLive = 100;
     void Start()
     {
         Money = startMoney;
-        Lives = 100;
+        Lives = lives_max;
     }
 
 
